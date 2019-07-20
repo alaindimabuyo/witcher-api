25a00e50d0204acbbd5f8f6d9d65a71a08a451b9
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-07-20 17:30:06 +0800

    error handling

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 //handle request
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 //product route
 const productRoutes = require("./api/routes/products");
+//orders route
+const orderRoutes = require("./api/routes/orders");
 
-//middleware
+app.use(morgan("dev"));
+
 app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+
+//error handling
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  //pass the error request
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
