26d6ace11dd9647c64fae5398a62c202c4b18a12
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-07-24 19:59:26 +0800

    user signup

---
+++ api/routes/user.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+      //mails match throw error
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "User already exist"
+        });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err
+            });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash
+            });
+            user
+              .save()
+              .then(result => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User Created"
+                });
+              })
+              .catch(err => {
+                res.status(500).json({
+                  error: err
+                });
+              });
+          }
+        });
+      }
+    });
+});
+
+router.delete("/:userId", (req, res) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: "User Deleted"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+module.exports = router;
--- index.js
+++ index.js
@@ -35,9 +35,10 @@ app.use((req, res, next) => {
 //Routes
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
-
+const userRoutes = require("./api/routes/user");
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 //error handling
 app.use((req, res, next) => {
