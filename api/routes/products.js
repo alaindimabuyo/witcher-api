b43d0be3a35919fe2691113fe0eb37ce74a18037
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-07-24 17:03:21 +0800

    images

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,10 +2,38 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Product = require("../models/product");
+const multer = require("multer");
+
+//image
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  }
+});
+//filter image types
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 router.get("/", (req, res, next) => {
   Product.find()
-    .select("name price _id")
+    .select("name price _id productImage")
     .exec()
     .then(docs => {
       //metada
@@ -16,6 +44,7 @@ router.get("/", (req, res, next) => {
             _id: doc._id,
             name: doc.name,
             price: doc.price,
+            productImage: doc.productImage,
             request: {
               type: "GET",
               url: "http://localhost:3000/products/" + doc._id
@@ -30,12 +59,15 @@ router.get("/", (req, res, next) => {
       res.status(500).json(doc);
     });
 });
-router.post("/", (req, res, next) => {
+
+router.post("/", upload.single("productImage"), (req, res, next) => {
+  console.log(req.file);
   //save to DB
   const product = new Product({
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
-    price: req.body.price
+    price: req.body.price,
+    productImage: " /uploads/" + req.file.filename
   });
   //store to db
   product
@@ -58,6 +90,7 @@ router.get("/:productID", (req, res, next) => {
   const id = req.params.productID;
 
   Product.findById(id)
+    .select("name price _id productImage")
     .exec()
     .then(doc => {
       res.status(200).json(doc);
--- index.js
+++ index.js
@@ -14,6 +14,10 @@ mongoose.connect(
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(morgan("dev"));
+//show image publicly
+app.use("/uploads", express.static("uploads"));
+
 //Cors Headers | prevent cors errors
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -34,8 +38,6 @@ const orderRoutes = require("./api/routes/orders");
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
-
-app.use(morgan("dev"));
 
 //error handling
 app.use((req, res, next) => {
